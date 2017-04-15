bf640953a98c07ac4681422103270bd1a9ef03bd
amitelran <amitelran2110@gmail.com>
2017-04-15 20:01:23 +0300

    queue and messages

---
+++ Assignment1/src/main/java/queueMessages/newTaskMessage.java
@@ -0,0 +1,12 @@
+package queueMessages;
+
+
+/* Message from the application to the manager 
+ * (location of an input file with a list of PDF URLs and opertaions to perform) 
+ * */
+public class newTaskMessage {
+	private  String inputFileLocation;
+	
+	
+	public 
+}
