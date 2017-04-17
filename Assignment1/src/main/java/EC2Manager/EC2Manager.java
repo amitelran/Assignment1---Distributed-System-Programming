c6fb50abc4c992f4d485dde8a03fa6337db8de98
amitelran <amitelran2110@gmail.com>
2017-04-17 18:56:48 +0300

    Manager and Workers update

--- Assignment1/src/main/java/EC2Manager/EC2Manager.java
+++ Assignment1/src/main/java/EC2Manager/EC2Manager.java
@@ -1,8 +1,10 @@
 package EC2Manager;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map.Entry;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
@@ -20,9 +22,17 @@ import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.Tag;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 
 import LocalApplication.LocalApplication;
 import S3.S3;
+import SQS.SimpleQueueService;
 
 public class EC2Manager {
 
@@ -31,9 +41,71 @@ public class EC2Manager {
         AWSCredentials credentials = new PropertiesCredentials(EC2Manager.class.getResourceAsStream("AwsCredentials.properties"));
         AmazonEC2 ec2 = new AmazonEC2Client(credentials);
         int numOfWorkers = 0;
+        String localApp_Manager_QueueURL = null;
 		
+        
+        /*	************** Check Local Application <--> Manager queue for messages **************	
+    	************** If received a message from local application, act accordingly **************	*/
+        
+        
+        AmazonSQS sqs = new AmazonSQSClient(new PropertiesCredentials(SimpleQueueService.class.getResourceAsStream("AwsCredentials.properties")));	// Declare SQS client
+        for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+        	String queueName = queueUrl.split("\\_")[0];			// Get queue name from beginning of queue URL up to '_' delimeter
+            if (queueName == "localAppQueue"){
+            	localApp_Manager_QueueURL = sqs.getQueueUrl(queueName).getQueueUrl();	// Get queue URL by queue name
+            }
+        	System.out.println("QueueUrl: " + queueUrl);
+        }
+        
+        
+        /*	************** Loop until termination message from local application **************	*/
+        
+        while(true){
+        	System.out.println("Receiving messages from " + localApp_Manager_QueueURL + " SQS queue\n");
+            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(localApp_Manager_QueueURL);
+            List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+            for (Message message : messages) {
+                System.out.println("  *** Message ***");
+                System.out.println("    MessageId:     " + message.getMessageId());
+                System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
+                System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
+                System.out.println("    Body:          " + message.getBody());
+                for (Entry<String, String> entry : message.getAttributes().entrySet()) {
+                    System.out.println("  Attribute");
+                    System.out.println("    Name:  " + entry.getKey());
+                    System.out.println("    Value: " + entry.getValue());
+                }
+                System.out.println();
+                if (message.getBody() == "newTask"){				// Received a "newTask" message from local application
+                	
+                	continue;
+                }
+                if (message.getBody() == "Terminate"){				// Received a termination message from local application
+                	break;
+                }
+            }
+        }
+        
+        
+        		
+        
+
+        
+        		
+        /*	************** Get input file from S3 Storage **************	*/
+        
+        
         AmazonS3 s3 = new AmazonS3Client(credentials);
-        File inputFile = s3.S3downloadObject
+		System.out.println("Downloading input file from S3 storage");
+        S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));		// Provide object information with GetObjectRequest
+        System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
+        //displayTextInputStream(object.getObjectContent());
+        InputStream inp = object.getObjectContent();								// Write input file contents to input stream
+        
+        
+        /*	************** Create Worker instances **************	*/
+        
+        
         try {
 			System.out.println("Creating Worker instance...");
 			RunInstancesRequest request = new RunInstancesRequest("ami-51792c38", 1, 1);
@@ -55,6 +127,11 @@ public class EC2Manager {
 	        System.out.println("Error Code: " + ase.getErrorCode());
 	        System.out.println("Request ID: " + ase.getRequestId());
         }
+        
+        
+        /*	************** Check for termination message  **************	*/
+        
+        
 	}
 
 }
--- Assignment1/src/main/java/Workers/Worker.java
+++ Assignment1/src/main/java/Workers/Worker.java
@@ -1,10 +1,77 @@
 package Workers;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.pdfbox.util.PDFImageWriter;
+import org.apache.pdfbox.util.PDFText2HTML;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.MessageAttributeValue;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+
+import SQS.SimpleQueueService;
+
+
 public class Worker {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 
+		AWSCredentials credentials = new PropertiesCredentials(Worker.class.getResourceAsStream("AwsCredentials.properties"));
+        AmazonEC2 ec2 = new AmazonEC2Client(credentials);
+        PDFImageWriter pdf_Img_Writer = new PDFImageWriter();
+        PDFText2HTML pdf_to_HTML_converter = new PDFText2HTML("UTF-8");
+        PDFTextStripper pdf_to_text_converter = new PDFTextStripper("UTF-8");
+        
+        String Manager_Workers_QueueURL = null;
+        AmazonSQS sqs = new AmazonSQSClient(new PropertiesCredentials(SimpleQueueService.class.getResourceAsStream("AwsCredentials.properties")));	// Declare SQS client
+        for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+        	String queueName = queueUrl.split("\\_")[0];								// Get queue name from beginning of queue URL up to '_' delimeter
+            if (queueName == "WorkersQueue"){
+            	Manager_Workers_QueueURL = sqs.getQueueUrl(queueName).getQueueUrl();	// Get queue URL by queue name
+            }
+        	System.out.println("QueueUrl: " + queueUrl);
+        }
+        
+        /*	************** Loop until termination message from manager **************	*/
+        
+        while(true){
+        	System.out.println("Receiving messages from " + Manager_Workers_QueueURL + " SQS queue\n");
+            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(Manager_Workers_QueueURL);
+            List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+            for (Message message : messages) {
+                System.out.println("  *** Message ***");
+                System.out.println("    MessageId:     " + message.getMessageId());
+                System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
+                System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
+                System.out.println("    Body:          " + message.getBody());
+                for (Entry<String, String> entry : message.getAttributes().entrySet()) {
+                    System.out.println("  Attribute");
+                    System.out.println("    Name:  " + entry.getKey());
+                    System.out.println("    Value: " + entry.getValue());
+                }
+                System.out.println();
+                if (message.getBody() == "newPDFTask"){				// Received a "newTask" message from local application
+                	Map<String, MessageAttributeValue> msgAttributes = message.getMessageAttributes();
+                	
+                	continue;
+                }
+                if (message.getBody() == "Terminate"){				// Received a termination message from local application
+                	break;
+                }
+            }
+        }
 	}
 
+	
+	
 }
